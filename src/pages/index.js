b7125e130b323599ebd387f88c5ad7f055498fb8
Kevin Cearns <kcearns@gmail.com>
2023-02-23 18:23:05 +0000

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 
 const IndexPage = () => {
+
+    axios.get('https://lucky-granita-b04374.netlify.app/.netlify/functions/hello')
+    .then(response => {
+        console.log(response.data)
+    })
+
+
+  
+
     return (
         <div>
         <h1>Hello world!</h1>
