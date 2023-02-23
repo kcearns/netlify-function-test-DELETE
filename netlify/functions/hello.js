10a77fc68eb26368a70e52dcc9f4897d2df49e38
Kevin Cearns <kcearns@gmail.com>
2023-02-23 18:26:29 +0000

    cors update

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,6 +2,9 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Hello World" }),
+      headers: {
+        "access-control-allow-origin": "*",
+      },
     };
   };
   
